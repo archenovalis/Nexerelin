041f015aeed62e2e46a7c9b04704b8525b06062f
archenovalis <archelaus.novalis@gmail.com>
2025-03-22 20:24:22 -0600

    uniformity

--- src/exerelin/world/industry/Commerce.java
+++ src/exerelin/world/industry/Commerce.java
@@ -8,22 +8,24 @@ public class Commerce extends IndustryClassGen {
 	public Commerce() {
 		super("commerce");
 	}
-	
+
 	@Override
 	public float getWeight(ProcGenEntity entity) {
-		//return entity.numProductiveIndustries / 2f;
-		return Misc.getNumIndustries(entity.market)/2f * getFactionMult(entity);
+		// return entity.numProductiveIndustries / 2f;
+		return Misc.getNumIndustries(entity.market) / 2f * getFactionMult(entity);
 	}
-	
+
 	@Override
 	public boolean canApply(ProcGenEntity entity) {
-		//if (entity.numProductiveIndustries >= NexMarketBuilder.getMaxProductiveIndustries(entity))
-		//	return false;
-		if (Misc.getNumIndustries(entity.market) >= Misc.getMaxIndustries(entity.market))
-			return false;
+		// if (entity.numProductiveIndustries >=
+		// NexMarketBuilder.getMaxProductiveIndustries(entity))
+		// return false;
+		// if (Misc.getNumIndustries(entity.market) >=
+		// Misc.getMaxIndustries(entity.market))
+		// return false;
 		return super.canApply(entity);
 	}
-	
+
 	@Override
 	public void apply(ProcGenEntity entity, boolean instant) {
 		super.apply(entity, instant);
